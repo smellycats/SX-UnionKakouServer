cea53c83883a452ba187f3807289f9a18673ad68
SmellyCat <smellycat2014@foxmail.com>
2018-04-18 11:36:30 +0800

    更改 移除无用的包

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import logging
 
 import arrow
 from flask import Flask, request, jsonify
-#from flask_restful import Api
-from flask_httpauth import HTTPBasicAuth, HTTPDigestAuth
-from flask_limiter import Limiter, HEADERS
+# from flask_restful import Api
+# from flask_httpauth import HTTPBasicAuth, HTTPDigestAuth
+# from flask_limiter import Limiter, HEADERS
 from flask.ext.sqlalchemy import SQLAlchemy
 # from flask.ext.cache import Cache
 
@@ -16,11 +16,11 @@ from my_logger import debug_logging, online_logging, access_logging
 # create a flask application - this ``app`` object will be used to handle
 app = Flask(__name__)
 app.config.from_object(Production())
-#api = Api(app)
+# api = Api(app)
 
 db = SQLAlchemy(app)
 
-auth = HTTPBasicAuth()
+#auth = HTTPBasicAuth()
 
 debug_logging(u'logs/error.log')
 access_logging(u'logs/access.log')
@@ -28,14 +28,16 @@ access_logging(u'logs/access.log')
 logger = logging.getLogger('root')
 access_logger = logging.getLogger('access')
 
+"""
 limiter = Limiter(app, headers_enabled=True, global_limits=["10/minute"])
 limiter.header_mapping = {
     HEADERS.LIMIT: "X-RateLimit-Limit",
     HEADERS.RESET: "X-RateLimit-Reset",
     HEADERS.REMAINING: "X-RateLimit-Remaining"
 }
+"""
 
-#cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+# cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 from . import views
 
@@ -43,16 +45,16 @@ from . import views
 @app.after_request
 def after_request(response):
     """访问信息写入日志"""
-    access_logger.info('%s - - [%s] "%s %s HTTP/1.1" %s %s'
-                       % (request.remote_addr,
-                          arrow.now().format('DD/MMM/YYYY:HH:mm:ss ZZ'),
-                          request.method, request.path, response.status_code,
-                          response.content_length))
+    info = u'{0} - - [{1}] "{2} {3} HTTP/1.1" {4} {5}'.format(
+        request.headers.get("X-Real-IP", request.remote_addr),
+        arrow.now('PRC').format('DD/MMM/YYYY:HH:mm:ss ZZ'), request.method,
+        request.path, response.status_code, response.content_length)
+    access_logger.info(info)
     response.headers['Server'] = app.config['HEADER_SERVER']
-    response.headers['Content-Type'] = 'application/json; charset=utf-8'
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+    #response.headers['Content-Type'] = 'application/json; charset=utf-8'
+    #response.headers['Access-Control-Allow-Origin'] = '*'
+    #response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+    #response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
 
     return response
 
